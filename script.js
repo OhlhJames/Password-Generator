6666f0462519c627bfc8c3ee7aa976fcbf08740a
IsItIzzet <ohlhjames@gmail.com>
2024-01-10 17:10:52 -0600

    added the ability to randomly choose a character and call it and added the ability to have a random password length

--- script.js
+++ script.js
@@ -17,7 +17,23 @@ function specialCharacter(){
     let i = Math.floor(Math.random() * (special.length -1))
     return special[i]
 }
+function passwordCharacter(){
+    let i = Math.floor(Math.random() * 3)
+    let characterChoice = i
+    if (characterChoice === 1){
+        return normalCharacter();
+    } else if (characterChoice === 2){
+        return caiptalizedCharacter();
+    } else {
+        return specialCharacter();
+    }
+}
 
+function passwordlength(min, max){
+        min = Math.ceil(8);
+        max = Math.floor(128);
+        return Math.floor(Math.random() * (max - min) + min);
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
