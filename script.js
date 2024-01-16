829c46bbb23c759f5beb558bc947636810912c7c
IsItIzzet <ohlhjames@gmail.com>
2024-01-15 21:11:57 -0600

    changed how code checks for types of characters needed, previous iteration of code did not output proper lengths of password given specifications because it would skip character outputs

--- script.js
+++ script.js
@@ -1,73 +1,52 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-specialConfirm = false
-uppercaseConfirm = false
-numeralConfirm = false
-lowercaseConfirm = false
+var passwordSuperArray = []
 function characterSelector(){
-    var characterConfirm = prompt("Please enter a character type you need (ex. Special, Number, Uppercase, Lowercase):", "enter type");
-    if (characterConfirm === "Special") {
-        specialConfirm = true;
-        var additionalConfirm = confirm("did you need more character types?")
-        if (additionalConfirm === true){
-            characterSelector()
-        }
-    }else if(characterConfirm === "Number") {
-        numeralConfirm = true;
-        var additionalConfirm = confirm("did you need more character types?")
-        if (additionalConfirm === true){
-            characterSelector()
-        }
-    }else if(characterConfirm === "Uppercase") {
-        uppercaseConfirm = true;
-        var additionalConfirm = confirm("did you need more character types?")
-        if (additionalConfirm === true){
-            characterSelector()
-        }
-    }else if(characterConfirm === "Lowercase"){
-        lowercaseConfirm = true
-        var additionalConfirm = confirm("did you need more character types?")
-        if (additionalConfirm === true){
-            characterSelector()
-        }
-    } else if (lowercaseConfirm === true && uppercaseConfirm === true && numeralConfirm === true && specialConfirm === true){
-        alert("all characters selected!")
-    }else {
-        alert("please select at least one character type");
+    var specialConfirm = confirm("did you need special characters?")
+    var numeralConfirm = confirm("did you need numeral characters?")
+    var uppercaseConfirm = confirm("did you need uppercase characters?")
+    var lowercaseConfirm = confirm("did you need lowercase characters?")
+    if (lowercaseConfirm !== true && uppercaseConfirm !== true && numeralConfirm !== true && specialConfirm !== true){
+        alert("Please select at least character type!")
         characterSelector();
+    }else if (lowercaseConfirm === true && uppercaseConfirm === true && numeralConfirm === true && specialConfirm === true){
+        passwordSuperArray = special.concat(numerals, lowercase, uppercase); 
+    }else if(lowercaseConfirm === true && uppercaseConfirm === true && numeralConfirm === true){
+        passwordSuperArray = numerals.concat(lowercase, uppercase)
+    }else if(uppercaseConfirm === true && numeralConfirm === true && specialConfirm === true){
+        passwordSuperArray = special.concat(numerals, uppercase)
+    }else if(lowercaseConfirm === true && numeralConfirm === true && specialConfirm === true){
+        passwordSuperArray = special.concat(numerals, lowercase)
+    }else if(lowercaseConfirm === true){
+        passwordSuperArray = lowercase
+    }else if(uppercaseConfirm === true){
+        passwordSuperArray = uppercase
+    }else if(numeralConfirm === true){
+        passwordSuperArray = numerals
+    }else if(specialConfirm === true){
+        passwordSuperArray = special
+    }else if(lowercaseConfirm === true && specialConfirm === true){
+        passwordSuperArray = lowercase.concat(special)
+    }else if(lowercaseConfirm === true && numeralConfirm === true){
+        passwordSuperArray = lowercase.concat(numerals)
+    }else if(lowercaseConfirm === true && uppercaseConfirm === true){
+        passwordSuperArray = lowercase.concat(uppercase)
+    }else if(uppercaseConfirm === true && numeralConfirm === true){
+        passwordSuperArray = uppercase.concat(numerals)
+    }else if(uppercaseConfirm === true && specialConfirm === true){
+        passwordSuperArray = uppercase.concat(special)
     }
-}
-characterSelector()
+    generatePassword()
+}   
+
 var minLength = prompt("How short can your password be:", "enter number")
 var maxLength = prompt("How long can your password be:", "enter number")
+var numerals =[0,1,2,3,4,5,6,7,8,9]
 var lowercase = [...'qwertyuiopasdfghjklzxcvbnm']
 var special = ['!', '@', '#', '$', '%', "^", "&", '*',]
 var uppercase = [... 'QWERTYUIOPASDFGHJKLZXCVBNM']
-function normalCharacter(){
-    let i = Math.floor(Math.random() * (lowercase.length-1))
-    return lowercase[i]
-}
-function caiptalizedCharacter(){
-    let i = Math.floor(Math.random() * (uppercase.length-1))
-    return uppercase[i]
-}
-function specialCharacter(){
-    let i = Math.floor(Math.random() * (special.length -1))
-    return special[i]
-}
+
 function passwordCharacter(){
-    let i = Math.floor(Math.random() * 4)
-    let characterChoice = i
-    if (characterChoice === 0 && numeralConfirm === true){
-        return Math.floor(Math.random() * 9);
-    } else if (characterChoice === 1 && specialConfirm === true){
-        return specialCharacter();
-    } else if (characterChoice === 2 && uppercaseConfirm === true){
-        return caiptalizedCharacter();
-    }else if (lowercaseConfirm === true) {
-        return normalCharacter();
-    
-    }
+    let i = Math.floor(Math.random() * passwordSuperArray.length-1)
+    return passwordSuperArray[i]
 }
 function passwordLength(){
         min = Math.ceil(minLength);
@@ -83,6 +62,8 @@ function generatePassword(){
     passwordString = passwordArray.join("");
     return passwordString;
 }
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -90,6 +71,5 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", characterSelector);
